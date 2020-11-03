dc5caf9b0c9d09dbeae2259fa15512743adfd4ef
Jeff McFadden <jeff@mcfadden.io>
2020-11-03 15:37:58 -0700

    Basic command sending.

--- lib/zway/client.rb
+++ lib/zway/client.rb
@@ -21,5 +21,26 @@ module Zway
       
       devices = devices_array.collect{ |d| Device.from_hash(d) }
      end
+     
+     
+     def send_command(device, command, params = [])
+      api_base   ||= Zway.api_base
+      session_id ||= Zway.session_id
+       
+      url_string = "#{api_base}/ZAutomation/api/v1/devices/#{device.id}/command/#{command}"
+      
+      url_string += params.map{ |k,v| "#{k}=#{v}" } if params.size > 0
+       
+      uri = URI( url_string )
+      req = Net::HTTP::Get.new(uri)
+      req['Cookie']="ZWAYSession=#{session_id}"
+      res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+        http.request(req)
+      end
+      
+      data = JSON.parse res.body
+      
+      raise "Command Error" unless data["code"] == 200
+     end
   end
 end
